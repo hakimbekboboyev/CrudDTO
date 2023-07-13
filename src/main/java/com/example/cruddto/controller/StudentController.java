91cebcd2e49a9ff3a329c09801e21b01516804ca
hakimbekboboyev <hakimbekboboyev@gmail.com>
2023-07-14 00:16:28 +0500

    First Commit

--- src/main/java/com/example/cruddto/controller/StudentController.java
+++ src/main/java/com/example/cruddto/controller/StudentController.java
@@ -1,13 +1,16 @@
 package com.example.cruddto.controller;
 
+import com.example.cruddto.dto.StudentDto;
+import com.example.cruddto.entity.Group;
 import com.example.cruddto.entity.Student;
 import com.example.cruddto.repository.GroupRepository;
 import com.example.cruddto.repository.StudentRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StudentController {
@@ -27,6 +30,45 @@ public class StudentController {
         }
         else {
             return ResponseEntity.ok(allStudent);
+        }
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity addStudent(@RequestBody StudentDto studentDto){
+        Optional<Group> groupById = groupRepository.findById(studentDto.getGroup_id());
+        if(groupById.isPresent()){
+            Student student = new Student();
+            student.setFirstName(studentDto.getFirstName());
+            student.setLastName(studentDto.getLastName());
+            student.setAge(studentDto.getAge());
+            student.setGroup(groupById.get());
+            studentRepository.save(student);
+            return ResponseEntity.ok("Successful");
+        }else {
+            return ResponseEntity.badRequest().body("Group Not Found!");
+        }
+    }
+
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity updateStudent(@PathVariable Integer id,
+                                        @RequestBody StudentDto studentDto){
+        Optional<Group> groupById = groupRepository.findById(studentDto.getGroup_id());
+        Optional<Student> studentById = studentRepository.findById(id);
+        if(groupById.isPresent()) {
+            if (studentById.isPresent()) {
+                Student student = studentById.get();
+                student.setFirstName(studentDto.getFirstName());
+                student.setLastName(studentDto.getLastName());
+                student.setAge(studentDto.getAge());
+                student.setGroup(groupById.get());
+                studentRepository.save(student);
+                return ResponseEntity.ok("Successful");
+            }else {
+                return ResponseEntity.badRequest().body("Student Not Found!");
+            }
+        }else {
+            return ResponseEntity.badRequest().body("Group Not Found!");
         }
     }
 
