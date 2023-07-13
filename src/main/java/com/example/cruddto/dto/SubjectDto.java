e583328f5fae50f5273352ce184faebff3a514d7
hakimbekboboyev <hakimbekboboyev@gmail.com>
2023-07-14 02:32:08 +0500

    First Commit

--- src/main/java/com/example/cruddto/dto/SubjectDto.java
+++ src/main/java/com/example/cruddto/dto/SubjectDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class SubjectDto {
-    private String name;
+    private String subjectName;
     private String firstName;
     private String lastName;
     private String category;
