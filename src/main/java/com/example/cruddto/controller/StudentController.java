c2b9fbce26b257ea1906a845f0c764b2b6b62630
hakimbekboboyev <hakimbekboboyev@gmail.com>
2023-07-14 10:45:56 +0500

    First Commit

--- src/main/java/com/example/cruddto/controller/StudentController.java
+++ src/main/java/com/example/cruddto/controller/StudentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class StudentController {
     final StudentRepository studentRepository;
     final GroupRepository groupRepository;
